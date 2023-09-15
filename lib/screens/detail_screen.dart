7f051b8e36895328488ce9dec44bd0242a986fbe
Daniel Hwang <humble92@gmail.com>
2023-09-15 01:44:21 -0400

    ADD: webtoon detail - future builder w/ id

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:webtoon/models/webtoon_model.dart';
+import 'package:webtoon/models/webtoon_detail_model.dart';
+import 'package:webtoon/models/webtoon_episode_model.dart';
+import 'package:webtoon/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   const DetailScreen({
     super.key,
     required this.webtoon,
   });
   final WebtoonModel webtoon;
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late final Future<WebtoonDetailModel> webtoonDetail =
+      ApiService.getToonById(widget.webtoon.id);
+  late final Future<List<WebtoonEpisodeModel>> episodes =
+      ApiService.getLatestEpisodesById(widget.webtoon.id);
+
+  // @override
+  // void initState() {
+  //   webtoonDetail =
+  //     ApiService.getToonById(widget.webtoon.id);
+  //   episodes =
+  //     ApiService.getLatestEpisodesById(widget.webtoon.id);
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +39,7 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         title: Text(
-          webtoon.title,
+          widget.webtoon.title,
           style: const TextStyle(
             fontSize: 24,
           ),
@@ -32,7 +54,7 @@ class DetailScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Hero(
-                tag: webtoon.id,
+                tag: widget.webtoon.id,
                 child: Container(
                   width: 250,
                   clipBehavior: Clip.hardEdge,
@@ -46,11 +68,42 @@ class DetailScreen extends StatelessWidget {
                       )
                     ],
                   ),
-                  child: Image.network(webtoon.thumb),
+                  child: Image.network(widget.webtoon.thumb),
                 ),
               ),
             ],
           ),
+          FutureBuilder(
+            future: webtoonDetail,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.only(
+                    left: 45,
+                    right: 45,
+                    top: 25,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                );
+              }
+              return const Text("...");
+            },
+          )
         ],
       ),
     );
